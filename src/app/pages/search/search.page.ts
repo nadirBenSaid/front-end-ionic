ce7e578780416bb80d2a6f1b43f8080d72650e6e
cimou97 <medachkour@gmail.com>
2019-05-29 00:10:36 +0000

    search page with add favorite button

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -15,38 +15,88 @@ export class SearchPage implements OnInit {
   searchTerm: string = '';
   type: SearchType = SearchType.all;
 
+  resu: any = [];
+  constructor(private searchService: SearchService, private router: Router, public storage: Storage) {
+    //connected user favorites
+    this.searchService.favories().subscribe(
+      (data) => {
+        this.resu = data;
+        console.log(this.resu);
+      },
+      (error) => {
+        console.log(error);
+      });
+  }
 
-    constructor(private searchService: SearchService, private router: Router, public storage: Storage) { }
+  //change button color if the emp is one of the users favourites
+  fColor;
+  colorRed() {
+    this.fColor = "danger";
+  }
+  colorBlue() {
+    this.fColor = "primary";
+  }
 
-  ngOnInit() { }
+  //to remove
+  fun1() { console.log('fun1') }
+  fun2() { console.log('fun2') }
 
-  id;
-  name="ios-star-outline";
-  changeIcon(id : any) {
-   if ((<HTMLInputElement>document.getElementById(id)).getAttribute("name")=="ios-star-outline") {
-    (<HTMLInputElement>document.getElementById(id)).setAttribute("name","ios-star");
-   }
-   else{
-    (<HTMLInputElement>document.getElementById(id)).setAttribute("name","ios-star-outline");
-   }
+  //add or remove favorites depending on the color (also change color on click)
+  addRemovefav(id) {
+    if ((<HTMLInputElement>document.getElementById("99" + id)).getAttribute("for") == "danger") {
+      this.searchService.removeFromFavourites(id).subscribe(data => {
+        console.log('removed');
+      }, error => {
+        console.log(error);
+      });
+      (<HTMLInputElement>document.getElementById("9" + id))
+        .setAttribute("class", "primary sc-ion-button-md-h md button button-solid ion-activatable ion-focusable hydrated button-small activated");
+      (<HTMLInputElement>document.getElementById("99" + id)).setAttribute("for", "primary");
+      if (this.newFav.indexOf(id) !== -1) this.newFav.splice(this.newFav.indexOf(id), 1); 
+    }
+    else {
+      this.searchService.addToFavourites(id).subscribe(data => {
+        console.log('added');
+      }, error => {
+        console.log(error);
+      });
+      (<HTMLInputElement>document.getElementById("9" + id))
+        .setAttribute("class", "danger sc-ion-button-md-h md button button-solid ion-activatable ion-focusable hydrated button-small activated");
+      (<HTMLInputElement>document.getElementById("99" + id)).setAttribute("for", "danger");
+      this.newFav.indexOf(id) === -1 ? this.newFav.push(id) : console.log("This item already exists");
+    }
   }
 
   data: Observable<any>;
-  result:any=[];
+  result: any = [];
+  newFav: any = [];
 
+  //turn the color of the button to red if it selected (serch for some thig than come back and search for it again)
+  lastfav() {
+    this.newFav.forEach(function (element) {
+      (<HTMLInputElement>document.getElementById("9" + element))
+      .setAttribute("class", "danger sc-ion-button-md-h md button button-solid ion-activatable ion-focusable hydrated button-small activated");
+      (<HTMLInputElement>document.getElementById("99" + element)).setAttribute("for", "danger");
+    });
+  }
+  
+  //gettin search data and calling lastfav after 2s
   getdata() {
     this.searchService.getSearchData(this.searchTerm).subscribe(
-      (data)=>{
-        console.log(data);
-        this.result=data;
+      (data) => {
+        this.result = data;
+        setTimeout(() => {
+          this.lastfav();
+        }, 2000);
       },
       (error) => {
         console.log(error);
       });
     }
-
+    
     logout() {
-        this.storage.clear();
-        this.router.navigateByUrl('/home');
+      this.storage.clear();
+      this.router.navigateByUrl('/home');
     }
+    ngOnInit() { }
 }
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
-import {environment} from '../../environments/environment';
+import { environment } from '../../environments/environment';
 
 // Typescript custom enum for search types (optional)
 export enum SearchType {
@@ -17,24 +17,38 @@ export enum SearchType {
 })
 export class SearchService {
 
-  latALon:string="";
-  constructor(private http: HttpClient, private geolocation:Geolocation) {
+  latALon: string = "";
+  constructor(private http: HttpClient, private geolocation: Geolocation) {
     this.location();
-   }
-
-  location()  {
-    this.geolocation.getCurrentPosition().then((resp)=>{
-      //resp.coords.latitude
-       //resp.coords.longitude
-       this.latALon = resp.coords.latitude.toFixed(4)+","+resp.coords.longitude.toFixed(4);
-
-   }).catch((error)=>{
-     console.log('error getting location',error);
-   });
   }
 
-  getSearchData(lieu:string): Observable<any>{
-    return this.http.get(environment.url+'/emplacements/query/'+lieu+'/100000/'+this.latALon, { withCredentials: true });  
+  location() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      //resp.coords.latitude
+      //resp.coords.longitude
+      this.latALon = resp.coords.latitude.toFixed(4) + "," + resp.coords.longitude.toFixed(4);
+
+    }).catch((error) => {
+      console.log('error getting location', error);
+    });
+  }
+
+  getSearchData(lieu: string): Observable<any> {
+    return this.http.get(environment.url + '/emplacements/query/' + lieu + '/100000/' + this.latALon, { withCredentials: true });
+  }
+
+  addToFavourites(empId) {
+    let postData = {
+    }
+    return this.http.post(environment.url + '/favories/add/' + empId, postData, { withCredentials: true });
+  }
+  favories() {
+    return this.http.get(environment.url + '/favories', { withCredentials: true });
+  }
+
+  removeFromFavourites(empId) {
+    return this.http.request('delete', environment.url + '/favories/delete/' + empId, { withCredentials: true });
+    //delete(environment.url + '/favories/delete/' + empId , { withCredentials: true });
   }
 
 }
